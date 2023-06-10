af47fb3a596f9c0ee1c4a3ad2688cbbffe584223
mehrab786 <mehrabtarrar@gmail.com>
2023-06-10 13:37:25 +0500

    that is final commit1

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -11,7 +11,7 @@ const page = async() => {
     <h2>CONTACT US</h2>
     <ContactCard />
       <section className={styles.contact_section}>
-        <h2>We'd love to hear <span>from you</span></h2>
+        <h2>Wed love to hear <span>from you</span></h2>
 
         <ContactForm/>
       </section>
